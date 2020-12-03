9c2d6dd52151044bdce759b82df32cf3664a511d
nicholas thompson <nicholasathompson@gmail.com>
2020-12-02 21:44:07 -0500

    test commit

--- 3/advent3.ps1
+++ 3/advent3.ps1
@@ -1,5 +1,5 @@
 <#
---- Day 2: Password Philosophy ---
+--- Day 2: Password Philosophy --- 2
 Your flight departs in a few days from the coastal airport; the easiest way down to the coast from here is via toboggan.
 
 The shopkeeper at the North Pole Toboggan Rental Shop is having a bad day. "Something's wrong with our computers; we can't log in!" You ask if you can take a look.
